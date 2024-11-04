590e575f18293f8e111a137ec0075d80b500ceeb
Dominique Ferreira <siqdominique99@gmail.com>
2024-11-04 17:28:27 -0300

    adicionando permissões nas requisicoes

--- src/main/java/com/meupet/api/Configuration/Security/SecurityConfig.java
+++ src/main/java/com/meupet/api/Configuration/Security/SecurityConfig.java
@@ -28,13 +28,27 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        // Acesso público (ADMIN, SECRETARIO e VETERINARIO) para login e registro
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/registro").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/vacinas").hasRole("VETERINARIO")
-                        .requestMatchers(HttpMethod.POST, "/nova-vacinacao").hasRole("SECRETARIO")
-                        .requestMatchers(HttpMethod.POST, "/novo-animal","/novo-proprietario").hasRole("SECRETARIO")
-                        .requestMatchers(HttpMethod.GET, "/animais").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/auth/registro").permitAll()
+
+                        // Permissões para Veterinários e Secretários
+                        .requestMatchers(HttpMethod.POST, "/animais/novo-animal", "/proprietarios/novo-proprietario").hasAnyRole("VETERINARIO", "SECRETARIO")
+                        .requestMatchers(HttpMethod.GET, "/animais", "/usuarios", "/proprietarios", "/historico-peso-altura").hasAnyRole("VETERINARIO", "SECRETARIO")
+                        .requestMatchers(HttpMethod.GET, "/animais/{id}", "/usuarios/{id}", "/proprietarios/{id}", "/historico-peso-altura/{id}").hasAnyRole("VETERINARIO", "SECRETARIO")
+                        .requestMatchers(HttpMethod.DELETE, "/animais/{id}", "/usuarios/{id}", "/proprietarios/{id}").hasAnyRole("VETERINARIO", "SECRETARIO")
+                        .requestMatchers(HttpMethod.PUT, "/usuarios/{id}", "/animais/{id}", "/proprietarios/{id}").hasAnyRole("VETERINARIO", "SECRETARIO")
+
+                        // Permissões exclusivas para Veterinários
+                        .requestMatchers(HttpMethod.POST, "/vacinas/nova-vacina", "/historico-peso-altura/novo-historico", "/vacinacoes/nova-vacinacao").hasRole("VETERINARIO")
+                        .requestMatchers(HttpMethod.GET, "/vacinacoes", "/vacinas").hasRole("VETERINARIO")
+                        .requestMatchers(HttpMethod.GET, "/vacinacoes/{id}", "/vacinas/{id}").hasRole("VETERINARIO")
+                        .requestMatchers(HttpMethod.DELETE, "/vacinacoes/{id}", "/vacinas/{id}", "/historico-peso-altura/{id}").hasRole("VETERINARIO")
+                        .requestMatchers(HttpMethod.PUT, "/vacinacoes/{id}", "/vacinas/{id}").hasRole("VETERINARIO")
+
+                        // Qualquer outra requisição exige autenticação
                         .anyRequest().authenticated()
+
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
